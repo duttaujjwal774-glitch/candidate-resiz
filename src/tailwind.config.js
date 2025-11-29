410fa2b4dcf281d1720a1fca90d32c24f96f5acd
duttaujjwal774-glitch <duttaujjwal774@gmail.com>
2025-11-29 19:57:57 +0530

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          700: "#0b4b7a",
+          800: "#083652"
+        }
+      }
+    }
+  },
+  plugins: []
+};
